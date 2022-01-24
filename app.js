ee4eaa63ed81e141f510ab1d7d5b7390b8adfbc7
eruc <eruc101010@gmail.com>
2022-01-25 00:36:40 +0800

    feat: add create short URL function.

--- app.js
+++ app.js
@@ -2,12 +2,40 @@ const express = require('express')
 const exhbs = require('express-handlebars')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+const shortUrlModule = require('./modules/shortUrlModule')
 
 app.engine('handlebars', exhbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+require('./config/mongoose')
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.render('index')
 })
 
-app.listen(port)
+app.post('/create/short', (req, res) => {
+  const fullUrl = req.body.fullUrl
+
+  shortUrlModule.findOne({ fullUrl: fullUrl }, function (err, result) {
+    if (err) { console.log('err') }
+    if (result) {
+      let shortUrl = result.shortUrl
+      res.render('createEnd', { shortUrl })
+    } else {
+      let randomNum = Math.random().toString(36).replace(/\.+/g, '').substring(1, 6)
+      let shortUrl = `http://shortUrl/${randomNum}`
+      shortUrlModule.create({ fullUrl, shortUrl })
+      res.render('createEnd', { shortUrl })
+    }
+  })
+})
+
+app.get('http://ldddd', (req, res) => {
+  res.send('https://tw.yahoo.com/')
+})
+
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`)
+})
