85891bf4c3b77589a255b66674b36a032ffe0a73
eruc <eruc101010@gmail.com>
2022-02-07 23:45:20 +0800

    refactor: improve create shortUrl code.

--- routes/modules/shortUrl.js
+++ routes/modules/shortUrl.js
@@ -5,34 +5,38 @@ const shortUrlModule = require('../../modules/shortUrlModule')
 
 router.post('/short', (req, res) => {
   const fullUrl = req.body.fullUrl
-
-  shortUrlModule.findOne({ fullUrl }, function (err, result) {
-    if (err) { console.log('err') }
-    if (!result) {
-      //資料庫沒有原始網址，製作新的短網址並存到資料庫中
-      const randomNum = Math.random().toString(36).replace(/\.+/g, '').substring(1, 6)
-      let shortUrl = `http://localhost:3000/${randomNum}`
-      shortUrlModule.create({ fullUrl, shortUrl },
-        function (err) {
-          if (err.code === 11000) {
-            let newShortUrl = shortUrl
-            while (shortUrl === newShortUrl) {
-              let newRandomNum = Math.random().toString(36).replace(/\.+/g, '').substring(1, 6)
-              newShortUrl = `http://localhost:3000/${newRandomNum}`
-            }
-            shortUrlModule.findByIdAndDelete({ fullUrl })
-            shortUrl = newShortUrl
-            shortUrlModule.create({ fullUrl, shortUrl })
-          }
-        })
-      res.redirect(`/create/short/repeat/${fullUrl}`)
-    } else {//資料庫已有原始網址，直接從資料庫取出
-      const shortUrl = result.shortUrl//這邊直接用有安全問題，handlebars會擋
-      res.render('createEnd', { shortUrl })
-    }
-  })
+  findFullUrl(fullUrl).then(shortUrl => res.render('createEnd', { shortUrl }))
 })
 
-
-
 module.exports = router
+
+async function findFullUrl(fullUrl) {
+  const result = await findshortUrl(fullUrl)
+  console.log(result)
+  const shortUrl = result.shortUrl
+  console.log(shortUrl)
+  return shortUrl
+}
+
+async function findshortUrl(fullUrl) {
+  let result
+  try {
+    result = await shortUrlModule.findOne({ fullUrl }).exec()
+    if (result !== null) { //當資料庫有資料時，直接回傳結果
+      return result
+    } else { //資料庫沒有時，新建資料
+      const randomNum = Math.random().toString(36).replace(/\.+/g, '').substring(1, 6)
+      let shortUrl = `http://localhost:3000/${randomNum}`
+      try {
+        await shortUrlModule.create({ fullUrl, shortUrl })//測試是否重複或新建
+        return shortUrlModule.findOne({ shortUrl })//成功新建後將資料回傳
+      } catch {//如果有重複，出現錯誤訊息，重新產生一個shortUrl
+        const randomNum = Math.random().toString(36).replace(/\.+/g, '').substring(1, 6)
+        let shortUrl = `http://localhost:3000/${randomNum}`
+        await shortUrlModule.create({ fullUrl, shortUrl })
+        return shortUrlModule.findOne({ shortUrl })
+      }
+    }
+  } catch {
+  }
+}
