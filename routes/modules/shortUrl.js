7cd4fa44087f617d43cf290de94297ca90b4472d
eruc <eruc101010@gmail.com>
2022-02-13 11:50:55 +0800

    test: test second findone.

--- routes/modules/shortUrl.js
+++ routes/modules/shortUrl.js
@@ -19,11 +19,17 @@ async function findshortUrl(fullUrl) {
     } else { //資料庫沒有時，新建資料
       let repeat = true //先設定重複判斷
       let shortUrl = ``
+      console.log(1)
       while (repeat) {
+        console.log(2)
         const randomNum = Math.random().toString(36).replace(/\.+/g, '').substring(1, 6)
+        console.log(3)
         shortUrl += `http://localhost:3000/${randomNum}`
-        let shortResult = await shortUrlModule.find({ shortUrl })//搜索資料庫確認重複(find的null 會導向catch)
-        repeat = repeat = shortResult.length >= 1 ? true : false //改變重複判斷
+        console.log(4)
+        let shortResult = await shortUrlModule.findone({ shortUrl }).exec()//搜索資料庫確認重複(find的null 會導向catch)
+        console.log(5)
+        repeat = shortResult !== null //改變重複判斷
+        console.log(6)
       }
       await shortUrlModule.create({ fullUrl, shortUrl })
       return shortUrl
