c54fedf9ae0d005f31d00ef86eae5f3be3d3bb2c
eruc <eruc101010@gmail.com>
2022-02-13 11:27:09 +0800

    refector: simplify code.

--- routes/modules/shortUrl.js
+++ routes/modules/shortUrl.js
@@ -5,33 +5,28 @@ const shortUrlModule = require('../../modules/shortUrlModule')
 
 router.post('/short', (req, res) => {
   const fullUrl = req.body.fullUrl
-  findFullUrl(fullUrl).then(shortUrl => res.render('createEnd', { shortUrl }))
+  findshortUrl(fullUrl).then(shortUrl => res.render('createEnd', { shortUrl }))
 })
 
 module.exports = router
 
-async function findFullUrl(fullUrl) {
-  const result = await findshortUrl(fullUrl)
-  const shortUrl = result.shortUrl
-  return shortUrl
-}
 
 async function findshortUrl(fullUrl) {
   try {
     const fullUrlResult = await shortUrlModule.findOne({ fullUrl }).exec()
     if (fullUrlResult !== null) { //當資料庫有資料時，直接回傳結果
-      return fullUrlResult
+      return fullUrlResult.shortUrl
     } else { //資料庫沒有時，新建資料
       let repeat = true //先設定重複判斷
       let shortUrl = ``
       while (repeat) {
         const randomNum = Math.random().toString(36).replace(/\.+/g, '').substring(1, 6)
         shortUrl += `http://localhost:3000/${randomNum}`
-        let shortResult = await shortUrlModule.find({ shortUrl })//搜索資料庫
-        repeat = shortResult.length >= 1 ? true : false //改變重複判斷
+        let shortResult = await shortUrlModule.find({ shortUrl })//搜索資料庫確認重複(find的null 會導向catch)
+        repeat = repeat = shortResult.length >= 1 ? true : false //改變重複判斷
       }
       await shortUrlModule.create({ fullUrl, shortUrl })
-      return shortUrlModule.findOne({ shortUrl })
+      return shortUrl
     }
   } catch {
     console.log('err')
